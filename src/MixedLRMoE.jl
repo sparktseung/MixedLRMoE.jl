2ff81482a0f471acf7f72bc92677bcf567fdf225
sparktseung <hi@sparktseung.com>
2023-05-31 03:00:42 -0400

    dep: InvertedIndices, funcs in LRMoE

--- src/MixedLRMoE.jl
+++ src/MixedLRMoE.jl
@@ -3,18 +3,21 @@ module MixedLRMoE
 # dependencies:
 import Base.Threads: @threads, nthreads, threadid
 import Base: @views
+import InvertedIndices: Not
 import LRMoE:
     @check_args,
     rowlogsumexp,
     params,
     exposurize_model,
-    sim_components
-    # _count_α,
-    # _count_params,
-    # loglik_exact,
-    # penalty_α,
-    # penalty_params,
-    # EM_M_expert_exact
+    sim_components,
+    EM_M_dQdα,
+    EM_M_dQ2dα2
+# _count_α,
+# _count_params,
+# loglik_exact,
+# penalty_α,
+# penalty_params,
+# EM_M_expert_exact
 import Distributions: MvNormal, Multinomial
 
 # export
@@ -23,5 +26,6 @@ import Distributions: MvNormal, Multinomial
 include("gating/logit.jl")
 include("simulation.jl")
 include("utils.jl")
+include("fit/em.jl")
 
 end
