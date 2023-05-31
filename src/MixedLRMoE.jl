6baa3ae12af86827e2685ed9c72230a2a0f844ed
sparktseung <hi@sparktseung.com>
2023-05-31 03:29:40 -0400

    various dependencies

--- src/MixedLRMoE.jl
+++ src/MixedLRMoE.jl
@@ -3,7 +3,10 @@ module MixedLRMoE
 # dependencies:
 import Base.Threads: @threads, nthreads, threadid
 import Base: @views
+import Distributions: MvNormal, Multinomial
 import InvertedIndices: Not
+import LinearAlgebra: I
+import Statistics: mean
 import LRMoE:
     @check_args,
     rowlogsumexp,
@@ -11,14 +14,13 @@ import LRMoE:
     exposurize_model,
     sim_components,
     EM_M_dQdα,
-    EM_M_dQ2dα2
-# _count_α,
-# _count_params,
-# loglik_exact,
-# penalty_α,
-# penalty_params,
-# EM_M_expert_exact
-import Distributions: MvNormal, Multinomial
+    EM_M_dQ2dα2,
+    loglik_exact,
+    penalty_α,
+    penalty_params,
+    EM_M_expert_exact,
+    _count_α,
+    _count_params
 
 # export
 
@@ -27,5 +29,6 @@ include("gating/logit.jl")
 include("simulation.jl")
 include("utils.jl")
 include("fit/em.jl")
+include("fit/fit_exact.jl")
 
 end
