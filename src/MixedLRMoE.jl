1c75e327d14ed0a6c4c19ed72204e777d5778955
sparktseung <hi@sparktseung.com>
2023-05-31 01:58:54 -0400

    dep: Distributions, @views

--- src/MixedLRMoE.jl
+++ src/MixedLRMoE.jl
@@ -1,5 +1,18 @@
 module MixedLRMoE
 
-# Write your package code here.
+# Dependencies:
+import Base.Threads: @threads, nthreads, threadid
+import Base: @views
+import LRMoE:
+    @check_args,
+    rowlogsumexp,
+    params,
+    _count_α,
+    _count_params,
+    loglik_exact,
+    penalty_α,
+    penalty_params,
+    EM_M_expert_exact
+import Distributions: MvNormal
 
 end
